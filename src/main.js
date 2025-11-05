d51c8f9957e4a2e015b938095856f5cd0369689c
amynx <andresyepezmn@gmail.com>
2025-11-05 11:54:01 -0500

    feat(load-recipe): base implementation for recipe loading and rendering
    - Initial working version that successfully loads and displays recipes
    - Code structure is temporary and will be refactored in upcoming commits
    - Includes initial setup of rendering logic, state handling, and alert feedback

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { init } from './scripts/controllers.js';
 import { createIcons, icons } from 'lucide';
 
 createIcons({ icons });
+init();
---
+++ src/scripts/model.js
@@ -0,0 +1,5 @@
+const state = {
+  recipe: {},
+  search: {},
+  bookmarks: {},
+};

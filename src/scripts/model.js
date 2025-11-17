f8c5b6922b747c1e252b2737f33a6a3194f93b5c
amynx <andresyepezmn@gmail.com>
2025-11-16 21:28:49 -0500

    refactor(model): move API requests and data formatting from the controller to the model
    - Fetch logic is implemented directly in the model.
    - Data received from the API is formatted before being stored.
    - Recipe status is centralized in the model.
    - The controller is no longer responsible for processing raw data.

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -3,3 +3,28 @@ const state = {
   search: {},
   bookmarks: {},
 };
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
+    if (!res.ok) throw new Error('Error Connection');
+    const { data } = await res.json();
+
+    const recipe = {
+      id: data.recipe.id,
+      imageUrl: data.recipe.image_url,
+      title: data.recipe.title,
+      cookingTime: data.recipe.cooking_time,
+      servings: data.recipe.servings,
+      ingredients: data.recipe.ingredients,
+      publisher: data.recipe.publisher,
+      sourceUrl: data.recipe.source_url,
+    };
+
+    state.recipe = recipe;
+    return state.recipe;
+  } catch (error) {
+    console.error('🚩Error while loading recipe:', error);
+    throw error;
+  }
+};
